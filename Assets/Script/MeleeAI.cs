ec59a9613ddd9efb3baba877e18d32c8e4c6044d
MNedkov09 <martin.nedkov2009@gmail.com>
2024-03-15 15:02:50 +0200

    MeleeAI Cooldown

--- Assets/Script/MeleeAI.cs
+++ Assets/Script/MeleeAI.cs
@@ -5,6 +5,7 @@ using Unity.AI;
 using UnityEngine.UI;
 using UnityEngine.AI;
 using Unity.VisualScripting;
+using System.Diagnostics;
 
 public class MeleeAI : MonoBehaviour
 {
@@ -15,34 +16,40 @@ public class MeleeAI : MonoBehaviour
     public float CooldownDuration = 1f;
     public float Damage;
     bool cooldown = true;
+    private Stopwatch timer=new Stopwatch();
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
         Cam = GameObject.Find("Main Camera");
         agent = gameObject.GetComponent<NavMeshAgent>();
+        Stopwatch timer = new Stopwatch();
+        this.timer.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Mathf.Abs(player.transform.position.x-transform.position.x) <= 30 && Mathf.Abs(player.transform.position.z - transform.position.z) <= 30)
+        if (Mathf.Abs(player.transform.position.x - transform.position.x) <= 30 && Mathf.Abs(player.transform.position.z - transform.position.z) <= 30)
         {
             AIHead.transform.LookAt(new Vector3(Cam.transform.position.x, AIHead.transform.position.y, Cam.transform.position.z));
             AIHead.transform.rotation = Quaternion.Euler(-90, AIHead.transform.rotation.eulerAngles.y, AIHead.transform.rotation.eulerAngles.z);
             agent.SetDestination(player.transform.position);
         }
-        if(Mathf.Abs(player.transform.position.x - transform.position.x) <= 6 && Mathf.Abs(player.transform.position.z - transform.position.z) <= 6)
+        if (Mathf.Abs(player.transform.position.x - transform.position.x) <= 6 && Mathf.Abs(player.transform.position.z - transform.position.z) <= 6)
         {
-            for(int i=0; i<animations.Count; i++)
+            for (int i = 0; i < animations.Count; i++)
             {
                 animations[i].Play("Base Layer.RoboAttack", 0);
-                
+
             }
-            if (cooldown)
+            
+           UnityEngine.Debug.Log(timer.ElapsedMilliseconds);
+            if (this.timer.ElapsedMilliseconds >= 1000)
             {
                 player.GetComponent<Health>().TakeDam(Damage);
-                StartCooldown();
+                this.timer.Restart();
+                this.timer.Start();
             }
         }
         else
